0a541e74b54fd91d6954af036c23ea6d50491712
Aseel Arafeh <arafehaseel@gmail.com>
2019-08-23 16:44:59 +0300

    Code for: Biography Class

--- src/builderdp/Biography.java
+++ src/builderdp/Biography.java
@@ -1,14 +1,56 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package builderdp;
 
-/**
- *
- * @author Aseel
- */
+
 public class Biography {
+    
+    private String licenseCountryName;
+    private String licenseNumber;
+    private int yearsInPractice;
+    private String gender;
+
+    public Biography(String licenseCountryName, String licenseNumber, int yearsInPractice, String gender) {
+        this.licenseCountryName = licenseCountryName;
+        this.licenseNumber = licenseNumber;
+        this.yearsInPractice = yearsInPractice;
+        this.gender = gender;
+    }
+
+    public String getLicenseCountryName() {
+        return licenseCountryName;
+    }
+
+    public void setLicenseCountryName(String licenseCountryName) {
+        this.licenseCountryName = licenseCountryName;
+    }
+
+    public String getLicenseNumber() {
+        return licenseNumber;
+    }
+
+    public void setLicenseNumber(String licenseNumber) {
+        this.licenseNumber = licenseNumber;
+    }
+
+    public int getYearsInPractice() {
+        return yearsInPractice;
+    }
+
+    public void setYearsInPractice(int yearsInPractice) {
+        this.yearsInPractice = yearsInPractice;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+
+    @Override
+    public String toString() {
+        return "Biography{" + "licenseCountryName=" + licenseCountryName + ", licenseNumber=" + licenseNumber + ", yearsInPractice=" + yearsInPractice + ", gender=" + gender + '}';
+    }
     
 }
