b96aeb4ffbbb49b9639c394ca00cecc42df3a1de
Aseel Arafeh <arafehaseel@gmail.com>
2019-08-23 16:37:01 +0300

    Create all needed classes

--- src/builderdp/Dentist.java
+++ src/builderdp/Dentist.java
@@ -9,13 +9,6 @@ package builderdp;
  *
  * @author Aseel
  */
-public class BuilderDP {
-
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String[] args) {
-        // TODO code application logic here
-    }
+public class Dentist {
     
 }
