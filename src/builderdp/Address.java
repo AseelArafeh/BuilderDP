b135ab7f4d87961f070f6f742b861fa03e09cda2
Aseel Arafeh <arafehaseel@gmail.com>
2019-08-23 16:39:56 +0300

    Code for: Address Class

--- src/builderdp/Address.java
+++ src/builderdp/Address.java
@@ -1,14 +1,67 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package builderdp;
 
-/**
- *
- * @author Aseel
- */
+
 public class Address {
+    
+    private String country;
+    private String state;
+    private String city;
+    private String street;
+    private int postalCode;
+
+    public Address(String country, String state, String city, String street, int postalCode) {
+        this.country = country;
+        this.state = state;
+        this.city = city;
+        this.street = street;
+        this.postalCode = postalCode;
+    }
+
+    public String getCountry() {
+        return country;
+    }
+
+    public void setCountry(String country) {
+        this.country = country;
+    }
+
+    public String getState() {
+        return state;
+    }
+
+    public void setState(String state) {
+        this.state = state;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getStreet() {
+        return street;
+    }
+
+    public void setStreet(String street) {
+        this.street = street;
+    }
+
+    public int getPostalCode() {
+        return postalCode;
+    }
+
+    public void setPostalCode(int postalCode) {
+        this.postalCode = postalCode;
+    }
+
+    @Override
+    public String toString() {
+        return "Address{" + "country=" + country + ", state=" + state + ", city=" + city + ", street=" + street + ", postalCode=" + postalCode + '}';
+    }
+    
     
 }
